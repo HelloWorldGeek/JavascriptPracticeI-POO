1119c7c7b16c3b9702a47528d021df8251da8a04
1valero <josevalerovegazo@gmail.com>
2016-04-30 13:31:46 -0500

    Ejercicios
    3

---
+++ Ejercicios/Problema3.js
@@ -0,0 +1,98 @@
+/*
+	Parametros
+*/
+var paramAnimal = {
+	nombre: "Luis",
+	energia: 120,
+	PoL: 1 //Es un perro o Loro poner 0 de perro o 1 de loro
+
+};
+/*
+	Clase Padre Animal
+*/
+function Animal(paramAnimal){
+	this.nombre = paramAnimal.nombre;
+	this.energia = paramAnimal.energia;
+}
+
+Animal.prototype.info = function(){
+	console.log("nombre: " + this.nombre + "\n" +"energia: " + this.energia + "\n");
+}
+
+Animal.prototype.ActualizarEnergia = function(energia){
+	this.energia = this.energia + energia;
+	console.log("Energia Actualizada");
+}
+
+Animal.prototype.Comer = function(){
+	var energia = 6;
+	console.log("Come\n");
+	return energia;
+}
+
+
+/*
+	Clase hija Perro
+*/
+
+function Perro(paramAnimal){
+	Animal.call(this, paramAnimal);
+}
+
+Perro.prototype = new Animal(this); 
+Perro.prototype.constructor = Perro;
+
+Perro.prototype.Correr = function(){
+	var energia = -5;
+	console.log("Corre " + this.nombre + "\n");
+	return energia;
+}
+
+/*
+	Clase hija Loro
+*/
+
+function Loro(paramAnimal){
+	Animal.call(this,paramAnimal);
+}
+
+Loro.prototype = new Animal(this);
+Loro.prototype.constructor = Loro;
+
+Loro.prototype.Volar = function(){
+	var volar = -8;
+	console.log("Vuela " + this.nombre + "\n");
+	return volar;
+}
+
+Loro.prototype.Comer = function(){
+	var AumentarEnergia = 10;
+	console.log("Come " + this.nombre + "\n");
+	return AumentarEnergia;
+}
+/*
+var newAnimal = new Animal(paramAnimal);
+newAnimal.info();
+newAnimal.ActualizarEnergia(newAnimal.Comer());
+newAnimal.info();
+*/
+
+
+if(paramAnimal.PoL == 0){
+	console.log("Es un Perro");
+	var newAnimal = new Perro(paramAnimal);
+	newAnimal.info();
+	newAnimal.ActualizarEnergia(newAnimal.Correr());
+	newAnimal.info();
+	}
+else if (paramAnimal.PoL == 1){
+	console.log("Es un loro");
+	var newAnimal = new Loro(paramAnimal);
+	newAnimal.info();
+	newAnimal.ActualizarEnergia(newAnimal.Volar());
+	newAnimal.info();
+	newAnimal.ActualizarEnergia(newAnimal.Comer());
+	newAnimal.info();
+	}
+else 
+	console.log("No es un animal");
